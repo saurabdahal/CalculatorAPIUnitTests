5ca40f1855f87fc66341184ca321fa6018a2ce1a
saurav dahal <d.saurab1@gmail.com>
2024-01-28 23:02:10 -0500

    initial commit before adding methods

--- CalculatorAPI/Controllers/Calculator.cs
+++ CalculatorAPI/Controllers/Calculator.cs
@@ -6,43 +6,6 @@ namespace CalculatorAPI.Controllers
     [Route("[controller]")]
     public class Calculator : ControllerBase
     {
-        [Route("/add/{num1}/{num2}")]
-        [HttpGet]
-        public float Add(float num1, float num2)
-        {
-
-            return num1 + num2;
-        }
-
-        [Route("/substract/{smallnumber}/{bignumber}")]
-        [HttpGet]
-        public float Substract(float smallnumber, float bignumber)
-        {
-            if(bignumber < smallnumber) {  return 0.0f;  }
-            return bignumber - smallnumber;
-        }
-
-        [Route("/multiply/{num1}/{num2}")]
-        [HttpGet]
-        public float Multiply(float num1, float num2)
-        {
-            return num1 * num2;
-        }
-
-        [Route("/divide/{smallnumber}/{bignumber}")]
-        [HttpGet]
-        public float Divide(float smallnumber, float bignumber)
-        {
-            if(bignumber < smallnumber) { return 0f; }
-            return bignumber / smallnumber;
-        }
-
-        [Route("/modulo/{smallnumber}/{bignumber}")]
-        [HttpGet]
-        public float Modulo(float smallnumber, float bignumber)
-        {
-            if (bignumber < smallnumber) { return 0f; }
-            return bignumber % smallnumber;
-        }
+        
     }
 }
