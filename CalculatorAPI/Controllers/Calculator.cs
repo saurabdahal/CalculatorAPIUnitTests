44424aa57220ce6fe2f97171e6e609d00fcf3ccb
saurav dahal <d.saurab1@gmail.com>
2024-01-28 23:05:24 -0500

    feat: added "Add" method that adds two parameters and returns the result.

--- CalculatorAPI/Controllers/Calculator.cs
+++ CalculatorAPI/Controllers/Calculator.cs
@@ -6,6 +6,12 @@ namespace CalculatorAPI.Controllers
     [Route("[controller]")]
     public class Calculator : ControllerBase
     {
-        
+        [Route("/add/{num1}/{num2}")]
+        [HttpGet]
+        public float Add(float num1, float num2)
+        {
+
+            return num1 + num2;
+        }
     }
 }
