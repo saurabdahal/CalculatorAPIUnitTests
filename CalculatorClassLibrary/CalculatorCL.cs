d91bdfb5c74e85320e97eef69ea15d4c7ed1cb44
saurav dahal <d.saurab1@gmail.com>
2024-02-10 23:42:37 -0500

    added calculator api controller methods to class library

---
+++ CalculatorClassLibrary/CalculatorCL.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CalculatorClassLibrary
+{
+    public class CalculatorCL
+    {
+        
+        public decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public decimal Subtract(decimal smallnumber, decimal bignumber)
+        {
+            if (bignumber < smallnumber) { return 0.0m; }
+            return bignumber - smallnumber;
+        }
+
+        public decimal Multiply(decimal num1, decimal num2)
+        {
+            return num1 * num2;
+        }
+
+        public decimal Divide(decimal smallnumber, decimal bignumber)
+        {
+            if (bignumber < smallnumber) { return 0m; }
+            return bignumber / smallnumber;
+        }
+
+        public decimal Modulo(decimal smallnumber, decimal bignumber)
+        {
+            if (bignumber < smallnumber) { return 0m; }
+            return bignumber % smallnumber;
+        }
+    }
+}
